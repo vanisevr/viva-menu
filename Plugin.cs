bf80442549cb3672821ee5155f85d65b8b6a9ab1
vanisevr <happyalexmin00@gmail.com>
2025-02-01 11:11:29 +0100

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -2,7 +2,7 @@ using BepInEx;
 using System;
 using UnityEngine;
 
-namespace iiMenu
+namespace viva meu
 {
     [System.ComponentModel.Description(PluginInfo.Description)]
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
@@ -10,10 +10,10 @@ namespace iiMenu
     {
         private void Start() // To that one dude that uses SMI to inject my menu, it's this method
         {
-            Console.Title = "ii's Stupid Menu // Build " + PluginInfo.Version;
+            Console.Title = "viva's menu // Build " + PluginInfo.Version;
 
             iiMenu.Patches.Menu.ApplyHarmonyPatches();
-            GameObject Loading = new GameObject("ii");
+            GameObject Loading = new GameObject("viva");
             Loading.AddComponent<iiMenu.UI.Main>();
             Loading.AddComponent<iiMenu.Notifications.NotifiLib>();
             Loading.AddComponent<iiMenu.Classes.CoroutineManager>();
